253cd6f3dfb4f7c1c20dd26ff4320a56609800e9
Min_CHk <142654773+kminchk@users.noreply.github.com>
2025-07-19 18:50:32 +0700

    q

--- src/index.js
+++ src/index.js
@@ -3,13 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(() => console.log("Service Worker Registered"));
-  });
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
